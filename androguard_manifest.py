07d4594c043a94dd1e26bebd7080bf8812dafc11
David Weinstein <dweinst@insitusec.com>
2015-04-17 15:58:30 -0400

    fix description

--- androguard_manifest.py
+++ androguard_manifest.py
@@ -2,7 +2,7 @@ from androguard.core.bytecodes.apk import *
 import argparse
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='ios-static analysis tool')
+    parser = argparse.ArgumentParser(description='androguard manifest dumper tool')
     parser.add_argument('path', help='apk file path')
     args = parser.parse_args()
     a = APK(args.path)
