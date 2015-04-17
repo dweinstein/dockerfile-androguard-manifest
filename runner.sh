566e83ea49aef0b8a18f79ae69fa4953d677624c
David Weinstein <dweinst@insitusec.com>
2015-04-17 16:03:14 -0400

    thx jake

--- runner.sh
+++ runner.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-APK_URL=$1
-CALLBACK_URL=$2
+APK_URL="$1"
+CALLBACK_URL="$2"
 
-curl -m 20 -s $APK_URL -o /tmp/android_app.apk
+curl -m 20 -s "${APK_URL}" -o /tmp/android_app.apk
 
 python ./androguard_manifest.py /tmp/android_app.apk | \
-  curl -m 5 -s -XPOST $CALLBACK_URL -H "Content-Type: text/xml" --data-binary @-
+  curl -m 5 -s -XPOST "${CALLBACK_URL}" -H "Content-Type: text/xml" --data-binary @-
 
