31afcdaaf2345cf9e5c9e17677d8dc3107501a86
David Weinstein <dweinst@insitusec.com>
2015-04-17 16:00:06 -0400

    remove spurious status

--- runner.sh
+++ runner.sh
@@ -6,7 +6,6 @@ CALLBACK_URL=$2
 
 curl -m 20 -s $APK_URL -o /tmp/android_app.apk
 
-STATUS=$?
 python ./androguard_manifest.py /tmp/android_app.apk | \
   curl -m 5 -s -XPOST $CALLBACK_URL -H "Content-Type: text/xml" --data-binary @-
 
