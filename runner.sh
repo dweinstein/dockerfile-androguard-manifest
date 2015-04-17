92b12404a0833ed6b6efd3f00bed6ac127860358
David Weinstein <dweinst@insitusec.com>
2015-04-17 16:20:56 -0400

    dump to stdout if not passed 2nd param

--- runner.sh
+++ runner.sh
@@ -7,5 +7,6 @@ CALLBACK_URL="$2"
 curl -m 20 -s "${APK_URL}" -o /tmp/android_app.apk
 
 python ./androguard_manifest.py /tmp/android_app.apk | \
-  curl -m 5 -s -XPOST "${CALLBACK_URL}" -H "Content-Type: text/xml" --data-binary @-
+  ( [[ -z "${CALLBACK_URL}" ]] && cat || \
+  curl -m 5 -s -XPOST "${CALLBACK_URL}" -H "Content-Type: text/xml" --data-binary @- )
 
