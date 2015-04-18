8764ec0b3d94ade99d6c13750f35bbe0065b205d
David Weinstein <dweinst@insitusec.com>
2015-04-18 13:56:47 -0400

    one less subshell

--- runner.sh
+++ runner.sh
@@ -9,7 +9,8 @@ CONTENT_TYPE="text/xml"
 TOOL="python ./androguard_manifest.py"
 
 # use a ramfs if possible for storing the app
-TMP_DIR=$([[ -d "/dev/shm" ]] && echo "/dev/shm/" || echo "/tmp/")
+[[ -d "/dev/shm" ]] && TMP_DIR="/dev/shm/" || TMP_DIR="/tmp/"
+
 INPUT_PATH="${TMP_DIR}/android_app.apk"
 
 ( ( [[ -z "${APK_URL}" ]] && cat > "${INPUT_PATH}" ) || \
